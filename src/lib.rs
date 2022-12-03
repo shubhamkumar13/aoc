292c03556a1229f0fdf749a53b69080f436f804c
Shubham <kumar.shubham1312@gmail.com>
2022-12-04 01:04:02 +0530

    restructure tests to include all the aocs

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,32 @@
 mod aoc_2022;
 
-use lazy_static::lazy_static;
-use std::{fs::File, io::Read, path::Path};
-
-use crate::aoc_2022::day1;
-use crate::aoc_2022::day2;
-use crate::aoc_2022::day3;
-
-fn parse_input(source: &str) -> String {
-    let path = Path::new(source);
-    let display = path.display();
-
-    let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {display}: {why}"),
-        Ok(file) => file,
-    };
-
-    let mut buf = String::new();
-    let _ = file.read_to_string(&mut buf);
-
-    buf
-}
-
-fn generate_input_path(s: &str) -> String {
-    let mut input_path = String::from("/home/sk/aoc/src/aoc_2022/input/");
-    input_path.push_str(s);
-    input_path
-}
-
 #[cfg(test)]
-mod tests {
-    use super::*;
+mod aoc_2022_tests {
+
+    use std::{fs::File, io::Read, path::Path};
+
+    use crate::aoc_2022::{day1, day2, day3};
+
+    fn parse_input(source: &str) -> String {
+        let path = Path::new(source);
+        let display = path.display();
+
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("couldn't open {display}: {why}"),
+            Ok(file) => file,
+        };
+
+        let mut buf = String::new();
+        let _ = file.read_to_string(&mut buf);
+
+        buf
+    }
+
+    fn generate_input_path(s: &str) -> String {
+        let mut input_path = String::from("/home/sk/aoc/src/aoc_2022/input/");
+        input_path.push_str(s);
+        input_path
+    }
 
     #[test]
     fn day1_example_p1() {
@@ -118,7 +114,6 @@ mod tests {
         let path = "day3_example_input.txt";
         let s = parse_input(generate_input_path(path).as_str());
         let result = day3::p2::solution(s);
-        // println!("{result}");
         assert_eq!(result, 70)
     }
 
